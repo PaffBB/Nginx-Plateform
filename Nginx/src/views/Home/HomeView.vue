b57ef57417a5114c91f8af41137440072d20288b
2691664562@qq.com <2691664562@qq.com>
2023-08-29 18:58:21 +0800

    导航2完成

---
+++ Nginx/src/views/Home/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomeViewBar from './HomeViewBar.vue';
+import AllInstance from './AllInstance.vue';
+</script>
+
+<template>
+<HomeViewBar></HomeViewBar>
+<AllInstance></AllInstance>
+</template>
+
+<style scoped>
+</style>
